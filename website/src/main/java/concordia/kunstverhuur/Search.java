5b2975b312463a1290d09410de3250e875ee8af0
Inf1n1te <timkerkhoven@live.nl>
2014-05-22 11:55:37 +0200

    stuff?

--- website/src/main/java/concordia/kunstverhuur/Search.java
+++ website/src/main/java/concordia/kunstverhuur/Search.java
@@ -12,11 +12,13 @@ public class Search extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public static void searchArtworks(String[] searchQueries) {
+		// Variables
 		Connection con = DB.getConnection();
 		Statement st = null;
 		ResultSet rs = null;
 		String query = null;
 
+		// Create search query
 		switch (searchQueries.length) {
 		case 0:
 			query = "SELECT * FROM Artwork";
@@ -25,16 +27,22 @@ public class Search extends HttpServlet {
 			query = searchQueries[0];
 			break;
 		default:
+			query = "(" + searchQueries[0] + ")";
+			for (int i = 1; i < searchQueries.length; i++) {
+				query += " INTERSECT (" + searchQueries[i] + ")";
+			}
 			break;
 		}
-		
-		
+
+		//Get data from database
 		try {
 			st = con.createStatement();
-			//rs = //TODO;
+			rs = st.executeQuery(query);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		
 	}
 }
